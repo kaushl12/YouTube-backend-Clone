0821d0ef202d28b4d13e2e2520d5f52b040d30a5
kaushl12 <kaushalchaudhari82@.com>
2025-06-05 15:16:34 +0530

    cloudinary conifg

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -64,7 +64,7 @@ userSchema.methods.isPasswordCorrect= async function (password) {
 userSchema.methods.generateAccessToken=function(){
    return jwt.sign({
         _id: this._id,
-        email: this.emial,
+        email: this.email,
         username:this.username,
         fullName:this.fullName
     },process.env.ACCESS_TOKEN_SECRET,{
